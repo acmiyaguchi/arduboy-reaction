721a016aa0993252965d0915e7293aabaa719b74
Anthony Miyaguchi <acmiyaguchi@gmail.com>
2019-06-25 00:08:22 -0700

    Refactor data export into a QRExporter class

--- main/main.ino
+++ main/main.ino
@@ -3,19 +3,18 @@
 
 // Deary-Liewald Task
 
-Arduboy2 arduboy;
-
 class QR {
  private:
   QRCode _encoder;
+  Arduboy2& _arduboy;
   // The size of the pixel for QR code
-  static const uint8_t _scale = 2;
+  const uint8_t _scale = 2;
   // side length is `4 * version + 17`, or 29
-  static const uint8_t _version = 3;
+  const uint8_t _version = 3;
 
   enum Position { left, right, center };
 
-  void draw(const uint8_t* data, Position position) {
+  void draw(const char* data, Position position) {
     uint8_t _data[qrcode_getBufferSize(_version)];
     qrcode_initText(&_encoder, _data, _version, ECC_MEDIUM, data);
 
@@ -39,25 +38,62 @@ class QR {
     for (uint8_t y = 0; y < _encoder.size; y++) {
       for (uint8_t x = 0; x < _encoder.size; x++) {
         bool color = qrcode_getModule(&_encoder, x, y);
-        arduboy.drawRect(x * _scale + x_off, y * _scale + y_off, _scale, _scale,
-                         color);
+        _arduboy.drawRect(x * _scale + x_off, y * _scale + y_off, _scale,
+                          _scale, color);
       }
     }
   }
 
  public:
-  void draw_center(const uint8_t* data) { draw(data, Position::center); }
-  void draw_left(const uint8_t* data) { draw(data, Position::left); }
-  void draw_right(const uint8_t* data) { draw(data, Position::right); }
+  QR(Arduboy2& arduboy) : _arduboy(arduboy) {}
+  void draw_center(const char* data) { draw(data, Position::center); }
+  void draw_left(const char* data) { draw(data, Position::left); }
+  void draw_right(const char* data) { draw(data, Position::right); }
 };
 
-QR qr;
-const uint8_t MAX_DATA = 5;
-char const* data[MAX_DATA] = {
-    "one fish", "two fish", "red fish", "blue fish", "fin",
+class QRExporter {
+ private:
+  Arduboy2& arduboy;
+  QR qr;
+  static const uint8_t MAX_DATA = 5;
+  char const* data[MAX_DATA] = {
+      "one fish", "two fish", "red fish", "blue fish", "fin",
+  };
+  uint8_t page = 0;
+  bool dirty = true;
+
+ public:
+  QRExporter(Arduboy2& arduboy) : arduboy(arduboy), qr(arduboy) {}
+  void execute() {
+    arduboy.pollButtons();
+    if (arduboy.justPressed(LEFT_BUTTON)) {
+      if (page > 0) {
+        page--;
+      }
+      dirty = true;
+    } else if (arduboy.justPressed(RIGHT_BUTTON)) {
+      if (page < MAX_DATA / 2) {
+        page++;
+      }
+      dirty = true;
+    }
+
+    if (dirty) {
+      arduboy.clear();
+      if (page == 2) {
+        qr.draw_center(data[2 * page]);
+      } else {
+        qr.draw_left(data[2 * page]);
+        qr.draw_right(data[2 * page + 1]);
+      }
+      arduboy.display();
+      dirty = false;
+    }
+  }
 };
-uint8_t page = 0;
-bool dirty = true;
+
+Arduboy2 arduboy;
+QRExporter qrexporter(arduboy);
 
 void setup() {
   arduboy.begin();
@@ -69,29 +105,5 @@ void loop() {
   if (!arduboy.nextFrame()) {
     return;
   }
-
-  arduboy.pollButtons();
-  if (arduboy.justPressed(LEFT_BUTTON)) {
-    if (page > 0) {
-      page--;
-    }
-    dirty = true;
-  } else if (arduboy.justPressed(RIGHT_BUTTON)) {
-    if (page < MAX_DATA / 2) {
-      page++;
-    }
-    dirty = true;
-  }
-
-  if (dirty) {
-    arduboy.clear();
-    if (page == 2) {
-      qr.draw_center(data[2 * page]);
-    } else {
-      qr.draw_left(data[2 * page]);
-      qr.draw_right(data[2 * page + 1]);
-    }
-    arduboy.display();
-    dirty = false;
-  }
+  qrexporter.execute();
 }
